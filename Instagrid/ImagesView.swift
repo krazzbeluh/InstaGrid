e33644553c6f953419b034700c334f48da71802d
Paul Leclerc <paulleclerc@icloud.com>
2019-04-04 18:57:31 +0200

    Corrected issue #2

--- Instagrid/ImagesView.swift
+++ Instagrid/ImagesView.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ImagesView: UIView {
-
     enum Disposition {
         case portrait, upsideDown, four
     }
@@ -40,16 +39,35 @@ class ImagesView: UIView {
         switch selectedButton {
         case .button1:
             button1.setImage(image, for: .normal)
+//            the 3 next lines are here to set the good image format. I can't set before because init images are in another format.
+            button1.contentHorizontalAlignment = .fill
+            button1.contentVerticalAlignment = .fill
+            button1.imageView?.contentMode = .scaleAspectFill
         case .button2:
             button2.setImage(image, for: .normal)
+            button2.contentHorizontalAlignment = .fill
+            button2.contentVerticalAlignment = .fill
+            button2.imageView?.contentMode = .scaleAspectFill
         case .button3:
             button3.setImage(image, for: .normal)
+            button3.contentHorizontalAlignment = .fill
+            button3.contentVerticalAlignment = .fill
+            button3.imageView?.contentMode = .scaleAspectFill
         case .button4:
             button4.setImage(image, for: .normal)
+            button4.contentHorizontalAlignment = .fill
+            button4.contentVerticalAlignment = .fill
+            button4.imageView?.contentMode = .scaleAspectFill
         case .largeButton1:
             largeButton1.setImage(image, for: .normal)
+            largeButton1.contentHorizontalAlignment = .fill
+            largeButton1.contentVerticalAlignment = .fill
+            largeButton1.imageView?.contentMode = .scaleAspectFill
         case .largeButton2:
             largeButton2.setImage(image, for: .normal)
+            largeButton2.contentHorizontalAlignment = .fill
+            largeButton2.contentVerticalAlignment = .fill
+            largeButton2.imageView?.contentMode = .scaleAspectFill
         }
     }
     
